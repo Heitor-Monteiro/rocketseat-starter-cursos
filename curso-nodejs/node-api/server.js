b799c3c9dc4df32f5fa5a9bc7187f53132d93658
thiberius <heitorssx2@gmail.com>
2020-04-27 14:27:32 -0300

    Adicionando CORS

--- curso-nodejs/node-api/server.js
+++ curso-nodejs/node-api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
@@ -7,6 +8,9 @@ const app = express();
 
 // Permite enviar para a aplicação, dados no formato JSON
 app.use(express.json());
+
+// Trecho que viabiliza o acesso de outros domínios.
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
