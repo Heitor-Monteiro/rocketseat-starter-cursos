9e5922ecc3ee05d893d9f2ef94aa4fe68fac41d5
Heitor <heitorssx2@gmail.com>
2020-05-01 12:09:46 -0300

    Armazenando no estado

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 export default class Main extends Component {
+  state = {
+    products: [],
+  };
+
   // O método componentDidMount é executado
   // assim que o componente for mostrado em tela.
   componentDidMount() {
@@ -11,10 +15,16 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get("/products");
 
-    console.log(response.data.docs);
+    this.setState({ products: response.data.docs });
   };
 
   render() {
-    return <h1>Hello Rocketseat</h1>;
+    return (
+      <div className="product-list">
+        {this.state.products.map((product) => (
+          <h2 key={product._id}>{product.title}</h2>
+        ))}
+      </div>
+    );
   }
 }
