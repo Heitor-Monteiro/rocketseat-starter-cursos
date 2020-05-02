42f57242027f3c72dbd0bc5f481c5c78d82ac53d
Heitor <heitorssx2@gmail.com>
2020-05-02 00:09:44 -0300

    Página anterior/próxima
    Codificação para habilitar a navegação na paginação.

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -6,6 +6,8 @@ import "./styles.css";
 export default class Main extends Component {
   state = {
     products: [],
+    productInfo: {},
+    page: 1,
   };
 
   // O método componentDidMount é executado
@@ -14,25 +16,55 @@ export default class Main extends Component {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get("/products");
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
   };
 
   render() {
-    const { product } = this.state;
+    const { products, page, productInfo } = this.state;
 
     return (
       <div className="product-list">
-        {this.state.products.map((product) => (
+        {products.map((product) => (
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
 
-            <a href="">Acessar</a>
+            <a href="#">Acessar</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+            Próximo
+          </button>
+        </div>
       </div>
     );
   }
